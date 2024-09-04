f7724a2fb2e7832349c1e101c39478bf7e06223e
Matthew Thompson <matthew.thompson@nasa.gov>
2024-09-04 10:14:50 -0400

    Force fcompiler

--- setup.py
+++ setup.py
@@ -32,15 +32,12 @@ class F2PyCommand(Command):
         os.chdir(os.path.join(build_lib, 'ffnet/fortran'))
         sources = ['ffnet.f', 'pikaia.f']
         modules = ['_ffnet', '_pikaia']
-        env = os.environ.copy()
-        myfc = {'FC': 'gfortran'}
-        env.update(myfc)
         for s, m in zip(sources, modules):
             cmd = ['f2py', '-m', m, '-c', s, '-fcompiler=gnu95']
             self.announce(
                 f'Compiling Fortran extension: {str(cmd)}',
                 level=distutils.log.INFO)
-            subprocess.check_call(cmd, env=env)
+            subprocess.check_call(cmd)
         os.chdir('../../../..')
 
 
